4cb4e1414548d3de41f9d24a0b3d0d75c77dd937
Paolo PhD Turing <paolo.bresolin.2@phd.unipd.it>
2026-06-30 16:50:36 +0000

    errors and execution times are computed and stored

--- src/run_lin_experiments.cpp
+++ src/run_lin_experiments.cpp
@@ -5,6 +5,26 @@
 #include <omp.h>
 #include <chrono>
 #include <filesystem>
+#include <utility>
+
+std::pair<std::vector<double>, double> run_method(UncertainGraph &uncertain_graph, const std::function<std::vector<double>(const PossibleWorld&)> &in_world_centrality_fn, int k, const std::vector<double> &baseline_centralities, std::mt19937 &rng) {
+
+    // run the input method
+    auto start_time = std::chrono::high_resolution_clock::now();
+    std::vector<double> estimated_centralities = mc_centralities_uncertain_graph(uncertain_graph, k, in_world_centrality_fn, rng);
+    auto end_time = std::chrono::high_resolution_clock::now();
+
+    // compute the centralities errors and add them to the table
+    std::vector<double> centralities_errors(uncertain_graph.n);
+    for (int u = 0; u < uncertain_graph.n; ++u)
+        centralities_errors[u] = std::abs(baseline_centralities[u] - estimated_centralities[u]);
+
+    // compute and append the time required to compute the centralities in seconds
+    std::chrono::duration<double> elapsed_time = end_time - start_time;
+
+    // return a pair with the errors and the elapsed time
+    return std::pair<std::vector<double>, double>(centralities_errors, elapsed_time.count());
+}
 
 void run_single_experiment(UncertainGraph &uncertain_graph, std::string &output_dir_path, int k_baseline, double c_ew, int k, int l, std::mt19937 &rng) {
 
@@ -19,7 +39,7 @@ void run_single_experiment(UncertainGraph &uncertain_graph, std::string &output_
 
     // table that will contain a column for each algorithm with a centrality value for each node
     std::vector<std::vector<double>> table;
-    table.reserve(n_methods);
+    table.reserve(n_methods - 1);    // we do not save the errors of the exact baseline with respect to itself, since they are 0
 
     // vector that will contain the execution times of the methods
     std::vector<double> exec_times;
@@ -32,16 +52,13 @@ void run_single_experiment(UncertainGraph &uncertain_graph, std::string &output_
 
     // run the exact algorithm on the input uncertain graph with a baseline (i.e., large) value of k
     std::cout << "Running exact with k = " << k_baseline << std::endl;
-    auto start_exact_baseline = std::chrono::high_resolution_clock::now();
+    auto start_time_baseline = std::chrono::high_resolution_clock::now();
     std::vector<double> exact_centralities_baseline = mc_centralities_uncertain_graph(uncertain_graph, k_baseline, exact_lin_world, rng);
-    auto end_exact_baseline = std::chrono::high_resolution_clock::now();
-
-    // append the centralities
-    table.push_back(exact_centralities_baseline);
+    auto end_time_baseline = std::chrono::high_resolution_clock::now();
 
     // compute and append the time required to compute the centralities
-    std::chrono::duration<double> elapsed_exact_baseline = end_exact_baseline - start_exact_baseline;
-    exec_times.push_back(elapsed_exact_baseline.count());
+    std::chrono::duration<double> elapsed_time_baseline = end_time_baseline - start_time_baseline;
+    exec_times.push_back(elapsed_time_baseline.count());
 
     // ---------- EXACT ----------
 
@@ -50,17 +67,12 @@ void run_single_experiment(UncertainGraph &uncertain_graph, std::string &output_
 
     // run the exact algorithm on the input uncertain graph with k
     std::cout << "Running exact with k = " << k << std::endl;
-    auto start_exact = std::chrono::high_resolution_clock::now();
-    std::vector<double> exact_centralities = mc_centralities_uncertain_graph(uncertain_graph, k, exact_lin_world, rng);
-    auto end_exact = std::chrono::high_resolution_clock::now();
+    auto [exact_errors, exact_elapsed_time] = run_method(uncertain_graph, exact_lin_world, k, exact_centralities_baseline, rng);
 
-    // append the centralities
-    table.push_back(exact_centralities);
-
-    // compute and append the time required to compute the centralities
-    std::chrono::duration<double> elapsed_exact = end_exact - start_exact;
-    exec_times.push_back(elapsed_exact.count());
-
+    // append the computed errors and the execution time of the current method
+    table.push_back(exact_errors);
+    exec_times.push_back(exact_elapsed_time);
+    
     // ---------- EW ----------
 
     // append the name of the method
@@ -74,18 +86,13 @@ void run_single_experiment(UncertainGraph &uncertain_graph, std::string &output_
         return ew_lin_world(world, l, c_ew, rng);
     };
 
-    // run the ew algorithm on the loaded uncertain graph
+    // run the ew algorithm on the input uncertain graph with k
     std::cout << "Running ew with k = " << k << ", l = " << l << " and c = " << c_ew << std::endl;
-    auto start_ew = std::chrono::high_resolution_clock::now();
-    std::vector<double> ew_centralities = mc_centralities_uncertain_graph(uncertain_graph, k, ew_lin_world_adapter, rng);
-    auto end_ew = std::chrono::high_resolution_clock::now();
+    auto [ew_errors, ew_elapsed_time] = run_method(uncertain_graph, ew_lin_world_adapter, k, exact_centralities_baseline, rng);
 
-    // append the centralities
-    table.push_back(ew_centralities);
-
-    // compute and append the time required to compute the centralities
-    std::chrono::duration<double> elapsed_ew = end_ew - start_ew;
-    exec_times.push_back(elapsed_ew.count());
+    // append the computed errors and the execution time of the current method
+    table.push_back(ew_errors);
+    exec_times.push_back(ew_elapsed_time);
 
     // ---------- SAVE THE RESULTS ----------
 
@@ -93,8 +100,11 @@ void run_single_experiment(UncertainGraph &uncertain_graph, std::string &output_
     std::filesystem::path output_dir(output_dir_path);
 
     // save the computed centralities in the output file in tsv format
-    std::filesystem::path centralities_path = output_dir / "centralities.tsv";
-    save_centralities_tsv(centralities_path, table, method_names);
+    std::filesystem::path centralities_path = output_dir / "errors.tsv";
+    std::vector<std::string> table_cols(n_methods - 1);
+    for (int i = 1; i < n_methods; ++i)
+        table_cols[i - 1] = method_names[i];
+    save_centralities_tsv(centralities_path, table, table_cols);
 
     // save the execution times of the methods in tsv format
     std::filesystem::path times_path = output_dir / "execution_times.tsv";
@@ -173,12 +183,12 @@ int main(int argc, char* argv[]) {
 
     // TODO:
     //     - implement the pps algorithm for the lin's index;
-    //     - add the implemented algorithm to this file and test it to see whether the output centralities make sense;
-    //     - change this code to to save the vectors of errors with respect to the exact baseline rather than the centralities and run to see whether the saved errors make sense;
+    //     - add the implemented algorithm to this file and test it to see whether the output errors make sense;
     //     - change this code to envelop the single experiment into a for that repeats the experiment n times.
     //       Each of the n single experiments returns the table with the errors and the vector with the running times.
     //       Once a single experiment is executed the errors and times are summed up across experiment repetitions.
     //       After that all single experiments have finished, the errors and times are averaged and saved to file;
+    //     - compute the expected error epsilon, once set c, l, k, delta from the theory and compare it with the experimental results;
     //     - implement everything for the harmonic centrality.
 
     return 0;
